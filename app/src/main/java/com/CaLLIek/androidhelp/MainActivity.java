351703bf2f738709cabbfd0a474cfe31b364ac58
Александр Паршин <parshin_sashek@mail.ru>
2019-11-28 22:47:54 +0200

    Start

---
+++ app/src/main/java/com/CaLLIek/androidhelp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.CaLLIek.androidhelp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
